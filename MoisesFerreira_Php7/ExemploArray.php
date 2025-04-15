52fd3209e296dbd2ce1722d03238197d0c0936c7
Moises João Ferreira <154389705+moisesferreirajj@users.noreply.github.com>
2025-04-15 17:03:23 -0300

    Create ExemploArray.php

---
+++ MoisesFerreira_Php7/ExemploArray.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo Array - PHP</title>
+</head>
+<body>
+    <?php
+    $dias = array('Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado');
+    echo $dias[1]. "<br>";
+    print_r($dias);
+    echo "<br>";
+    var_dump($dias);
+    ?>
+
+    <footer>
+        <h2 align="center">Moises Ferreira | TDESN V3 | SENAI NORTE</h2>
+    </footer>
+</body>
+</html>
