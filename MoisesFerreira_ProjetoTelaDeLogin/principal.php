cd0b67680919c76bd62c94cfca32f7dcbc5de4f3
Moises João Ferreira <154389705+moisesferreirajj@users.noreply.github.com>
2025-05-21 17:03:03 -0300

    Bebessauro

--- MoisesFerreira_ProjetoTelaDeLogin/principal.php
+++ MoisesFerreira_ProjetoTelaDeLogin/principal.php
@@ -19,74 +19,150 @@ require_once 'conexao.php';
 
     // DEFINIÇÃO DAS PERMISSÕES POR PERFIL
     $permissoes = [
-        //ADMINISTRADOR
-        '1' =>
-        ['Cadastrar' =>[
-            "cadastro_usuario.php",
-            "cadastro_perfil.php",
-            "cadastro_cliente.php",
-            "cadastro_fornecedor.php",
-            "cadastro_produto.php",
-            "cadastro_funcionario.php"
-        ] ],
-        ['Buscar' =>[
-            "buscar_usuario.php",
-            "buscar_perfil.php",
-            "buscar_cliente.php",
-            "buscar_fornecedor.php",
-            "buscar_produto.php",
-            "buscar_funcionario.php"
-        ] ],
-        ['Alterar' =>[
-            "alterar_usuario.php",
-            "alterar_perfil.php",
-            "alterar_cliente.php",
-            "alterar_fornecedor.php",
-            "alterar_produto.php",
-            "alterar_funcionario.php"
-        ] ],
-        ['Excluir' =>[
-            "excluir_usuario.php",
-            "excluir_perfil.php",
-            "excluir_cliente.php",
-            "excluir_fornecedor.php",
-            "excluir_produto.php",
-            "excluir_funcionario.php"
-        ] ],
+        // ADMINISTRADOR
+        '1' => [
+            'Cadastrar' => [
+                "cadastro_usuario.php",
+                "cadastro_perfil.php",
+                "cadastro_cliente.php",
+                "cadastro_fornecedor.php",
+                "cadastro_produto.php",
+                "cadastro_funcionario.php"
+            ],
+            'Buscar' => [
+                "buscar_usuario.php",
+                "buscar_perfil.php",
+                "buscar_cliente.php",
+                "buscar_fornecedor.php",
+                "buscar_produto.php",
+                "buscar_funcionario.php"
+            ],
+            'Alterar' => [
+                "alterar_usuario.php",
+                "alterar_perfil.php",
+                "alterar_cliente.php",
+                "alterar_fornecedor.php",
+                "alterar_produto.php",
+                "alterar_funcionario.php"
+            ],
+            'Excluir' => [
+                "excluir_usuario.php",
+                "excluir_perfil.php",
+                "excluir_cliente.php",
+                "excluir_fornecedor.php",
+                "excluir_produto.php",
+                "excluir_funcionario.php"
+            ]
+        ],
 
-        //SECRETARIA
-        '2' =>
-        ['Cadastrar' =>[
-            "cadastro_cliente.php"
-        ] ],
-        ['Buscar' =>[
-            "buscar_cliente.php",
-            "buscar_fornecedor.php",
-            "buscar_produto.php"
-        ] ],
-        ['Alterar' =>[
-            "alterar_cliente.php",
-            "alterar_fornecedor.php"
-        ] ],
+        // SECRETARIA
+        '2' => [
+            'Cadastrar' => [
+                "cadastro_cliente.php"
+            ],
+            'Buscar' => [
+                "buscar_cliente.php",
+                "buscar_fornecedor.php",
+                "buscar_produto.php"
+            ],
+            'Alterar' => [
+                "alterar_cliente.php",
+                "alterar_fornecedor.php"
+            ]
+        ],
 
-        //ALMOXARIFADO
-        '3' =>
-        ['Cadastrar' =>[
-            "cadastro_fornecedor.php",
-            "cadastro_produto.php"
-        ] ],
-        ['Buscar' =>[
-            "buscar_fornecedor.php",
-            "buscar_produto.php"
-        ] ],
-        ['Alterar' =>[
-            "alterar_fornecedor.php",
-            "alterar_produto.php"
-        ] ],
-        ['Excluir' =>[
-            "excluir_fornecedor.php",
-            "excluir_produto.php"
-        ] ],
+        // ALMOXARIFADO
+        '3' => [
+            'Cadastrar' => [
+                "cadastro_fornecedor.php",
+                "cadastro_produto.php"
+            ],
+            'Buscar' => [
+                "buscar_usuario.php",
+                "buscar_perfil.php",
+                "buscar_cliente.php"
+            ],
+            'Alterar' => [
+                "alterar_usuario.php",
+                "alterar_perfil.php",
+                "alterar_cliente.php",
+                "alterar_fornecedor.php",
+                "alterar_produto.php",
+                "alterar_funcionario.php"
+            ],
+            'Excluir' => [
+                "excluir_usuario.php",
+                "excluir_perfil.php",
+                "excluir_cliente.php",
+                "excluir_fornecedor.php",
+                "excluir_produto.php",
+                "excluir_funcionario.php"
+            ]
+        ],
 
+        // OUTRO PERFIL (EXEMPLO)
+        '4' => [
+            'Cadastrar' => [
+                "cadastro_cliente.php"
+            ],
+            'Buscar' => [
+                "buscar_produto.php"
+            ],
+            'Alterar' => [
+                "alterar_cliente.php"
+            ]
         ]
+
+    ];
+
+    // OBTENDO AS OPÇÕES DISPONÍVEIS PARA O PERFIL DO USUÁRIO LOGADO
+    $opcoes_menu = $permissoes[$id_perfil];
+
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel - Principal</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <header>
+        <div class="saudacao">
+            <h2>
+                Bem-vindo(a), <?php echo $_SESSION['usuario']; ?>!
+                <br>
+                Perfil: <?php echo $nome_perfil; ?>
+            </h2>
+        </div>
+
+        <div class="logout">
+            <form action="logout.php" method="POST">
+                <button type="submit">Logout</button>
+            </form>
+        </div>
+    </header>
+
+    <nav>
+        <ul class="menu">
+            <?php foreach ($opcoes_menu as $categoria => $arquivos): ?>
+                <li class="dropdown">
+                    <a href="#"><?php echo $categoria; ?></a>
+                    <ul class="dropdown-menu">
+                        <?php foreach ($arquivos as $arquivo): ?>
+                            <li>
+                                <a href="<?php echo $arquivo; ?>">
+                                    <?php echo ucfirst(str_replace('_', ' ', basename($arquivo, '.php') )); ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+</body>
+</html>
